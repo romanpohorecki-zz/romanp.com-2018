f082e276fc8df0358bf4314f4ae6308fec634f4b
Roman Pohorecki <romanp@gmail.com>
2018-06-02 09:11:32 -0700

    Mobile CSS, modified template to accomodate variance (see doc)

--- includes/nav-1.php
+++
@@ -1,5 +0,0 @@
-				<nav class='nav-1 width-100 text-center text-caps'>
-					<a href='index.php#matte-painting'>matte painting</a>
-					<a href='canvas-and-print.php' class='margin-left-20' href=''>canvas &amp print</a>
-					<a href='about.php' class='margin-left-20' href=''>about</a>
-				</nav>
